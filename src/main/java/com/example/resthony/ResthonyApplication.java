fa380d5c30cd4e57f5dd64be413615be10eb7a28
jeremygregoire0 <jeremy.gregoire0@gmail.com>
2021-09-29 18:38:54 +0200

    Add files via upload

---
+++ src/main/java/com/example/resthony/ResthonyApplication.java
@@ -0,0 +1,13 @@
+package com.example.resthony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResthonyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResthonyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/resthony/ResthonyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resthony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResthonyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
