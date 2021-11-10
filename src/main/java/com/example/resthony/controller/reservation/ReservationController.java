0ea122826c8cc2681dd8d58f9f3bc3352c93fbf3
thibaut <th.cravatte@outlook.com>
2021-11-10 09:36:28 -0800

    :))

--- src/main/java/com/example/resthony/controller/reservation/ReservationController.java
+++ src/main/java/com/example/resthony/controller/reservation/ReservationController.java
@@ -7,12 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/admin")
+@RequestMapping("/admin/reservation")
 public class ReservationController {
     @Autowired
     private ReservationsDetailsServiceImpl service;
@@ -21,7 +23,7 @@ public class ReservationController {
     @GetMapping
     public String reservationPage(Model model) {
 
-        return "reservation/index";
+        return "admin/reservations";
     }
 
 
@@ -30,7 +32,23 @@ public class ReservationController {
 
         List<Reservation> listReservations = service.listAll();
         model.addAttribute("listUsers", listReservations);
-        return "admin/users";
+        return "admin/reservations";
+
+    }
+
+    @PostMapping("/save")
+    public String saveReservation(Reservation reservation, RedirectAttributes ra) {
+        service.save(reservation);
+        ra.addFlashAttribute("message", "vous avez bien réservé");
+        return "/index";
+    }
+
+
+    @PostMapping("edit")
+    public String editUser(Reservation reservation, RedirectAttributes ra) {
+        service.save(reservation);
+        ra.addFlashAttribute("message", "La modification a été réalisée");
+        return "redirect:/admin/users";
 
     }
 }
