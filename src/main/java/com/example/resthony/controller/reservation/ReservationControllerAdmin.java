cb2b4c21855eba908861edcfc45b1498d0589cba
Thibaut <th.cravatte@outlook.com>
2021-11-16 23:49:11 +0100

    delete + user controller

--- src/main/java/com/example/resthony/controller/reservation/ReservationControllerAdmin.java
+++ src/main/java/com/example/resthony/controller/reservation/ReservationControllerAdmin.java
@@ -4,10 +4,12 @@ package com.example.resthony.controller.reservation;
 import com.example.resthony.entities.Reservation;
 import com.example.resthony.entities.User;
 import com.example.resthony.services.impl.ReservationsDetailsServiceImpl;
+import com.example.resthony.services.impl.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -16,7 +18,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/admin/reservation")
-public class ReservationController {
+public class ReservationControllerAdmin {
     @Autowired
     private ReservationsDetailsServiceImpl service;
 
@@ -40,10 +42,23 @@ public class ReservationController {
 
 
     @PostMapping("edit")
-    public String editUser(Reservation reservation, RedirectAttributes ra) {
+    public String editReservation(Reservation reservation, RedirectAttributes ra) {
         service.save(reservation);
         ra.addFlashAttribute("message", "La modification a été réalisée");
-        return "redirect:/admin/users";
+        return "redirect:/admin/reservations";
 
+    }
+
+
+    @GetMapping("/delete/{id}")
+    public String deleteReservation(@PathVariable("id") Integer id, RedirectAttributes ra) {
+        try {
+            service.delete(id);
+
+        } catch (UserNotFoundException e) {
+
+        }
+        ra.addFlashAttribute("message", "la Reservationr a été suppriméé ");
+        return "redirect:/admin/reservations";
     }
 }
