a6d0aecacda484a5d8173ebdc3418c7fc8863503
Thibaut <th.cravatte@outlook.com>
2021-12-07 14:32:34 +0100

    commit 1 par 1

--- target/classes/db/migration/V1_0_0__init.sql
+++ target/classes/db/migration/V1_0_0__init.sql
@@ -13,8 +13,10 @@ create table users (
                        lastname varchar(255) not null,
                        password varchar(255) not null,
                        username varchar(255) not null,
-                       email text,
+                       email text not null,
+                       phone text,
                        resto bigint,
+                       contact varchar(15),
                        primary key (id_user)
 ) engine=InnoDB;
 
